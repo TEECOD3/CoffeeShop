8cbf1707908dfdde848b3b8fcfdb58d7ff3ede1f
temitope david <temi236topemicheal@gmail.com>
2023-04-19 14:28:22 +0100

    ✨responsive battle

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,24 @@ export default {
         coffeabout: "url('./public/Images/coffeeAboutSec.png')",
         coffeabout2: "url('./public/Images/cofeeAbout2.png')",
       },
-    },
-    fontFamily: {
-      inter: ["lato", "sans-serif"],
-      playfair: ["Playfair Display", "sans-serif"],
-    },
-    BackgroundColors: {
-      coffee: "#BC9A6C",
+      fontFamily: {
+        inter: ["lato", "sans-serif"],
+        playfair: ["Playfair Display", "sans-serif"],
+        lora: ["lora", "sans-serif"],
+        nunito: ["nunito", "sans-serif"],
+      },
+      colors: {
+        coffee: "#BC9A6C",
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
     },
   },
   plugins: [],
