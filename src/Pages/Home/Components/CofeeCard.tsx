bf7e0740bdcc3d42aaf9754922117508a1b01ee4
temitope david <temi236topemicheal@gmail.com>
2023-04-25 23:13:37 +0100

    latest stuff

---
+++ src/Pages/Home/Components/CofeeCard.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface CofeecardProps {}
+
+const Cofeecard: FC<CofeecardProps> = () => {
+  return <div className="">COFEECARD</div>;
+};
+
+export default Cofeecard;
--- src/Pages/Home/Components/RightCofeeItem.tsx
+++ src/Pages/Home/Components/RightCofeeItem.tsx
@@ -8,16 +8,16 @@ type rightcoffeitemtype = {
 const RightCoffeeItem = (props: rightcoffeitemtype) => {
   const { image, cofee, text } = props;
   return (
-    <li className="flex gap-2 md:gap-4 md:w-full">
-      <div className=" w-full max-sm:w-[70%] max-md:w-[80%]">
-        <h1 className="mb-2 text-xl font-bold text-[#4F4F4F] sm:text-2xl xl:text-3xl capitalize ">
+    <li className="flex md:gap-4 md:w-full">
+      <div className=" w-full max-md:w-[80%] ml-4">
+        <h1 className=" text-xl font-bold text-[#4F4F4F] sm:text-2xl xl:text-3xl capitalize   ">
           {cofee}
         </h1>
         <p className="text-[#4F4F4F] text-medium text-sm md:text-base">
           {text}
         </p>
       </div>
-      <div className="relative mt-2">
+      <div className="relative mt-2 ">
         <Ringer className="h-16 hidden md:block " />
         <div className="md:absolute md:top-[0.9rem] md:left-[1.3rem] ">
           {image}
