6b52801ded6d811d2ca9297389c3748485985aeb
temitope david <temi236topemicheal@gmail.com>
2023-05-06 22:02:13 +0100

    setting up blog page

--- src/Pages/Menu/Components/MenuCards.tsx
+++ src/Pages/Menu/Components/MenuCards.tsx
@@ -8,14 +8,14 @@ const Menucards: FC<MenucardsProps> = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   return (
-    <div className="relative w-full lg:mb-4 cursor-pointer z-20 hover:scale-[1.02] transition duration-100 delay-75 group ">
-      <div className="w-full relative">
-        <div className="absolute h-full w-full bg-black/20 opacity-0 group-hover:opacity-100"></div>
-        <div className="min-h-200px">
+    <div className="  border-2 p-2 relative w-full lg:mb-4 cursor-pointer z-20 hover:scale-[1.02] transition duration-100 delay-75 group ">
+      <div className=" md:w-[200px] relative mx-auto">
+        <div className="absolute h-full w-full round-xl bg-black/20 opacity-0 group-hover:opacity-100"></div>
+        <div className="  flex items-center justify-center">
           <img
             src={image}
             alt=""
-            className="object-cover bg-cover bg-top max-w-100px mx-auto"
+            className="object-cover bg-cover bg-top max-w-[100px] mx-auto"
           />
         </div>
       </div>
@@ -27,13 +27,13 @@ const Menucards: FC<MenucardsProps> = () => {
         <s className="text-lightdark text-sm">$900</s>
       </div>
 
-      <div className="button| absolute p-2  w-1/2 flex items-center justify-center  gap-4 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2  opacity-0 group-hover:opacity-100 group-hover:right-2 transition-all duration-300 ">
-        <div className=" p-1 lg:p-3 bg-white rounded-full flex items-center justify-center">
-          <ShoppingBagIcon className="h-4 w-4 lg:h-full lg:w-full  " />
+      <div className="button| absolute p-2  w-1/2 flex items-center justify-center top-1/3  gap-4 left-[45%] md:bottom-4 md:left-[43%]  -translate-x-1/3 -translate-y-1/3  opacity-0 group-hover:opacity-100 group-hover:right-2 transition-all duration-300 ">
+        <div className=" p-3 lg:p-2 shadow-xl bg-white/80 rounded-full flex items-center justify-center">
+          <ShoppingBagIcon className="h-4 w-4 lg:h-6 lg:w-6 " />
         </div>
-        <div className=" p-1 lg:p-3 bg-white rounded-full flex items-center justify-center">
+        <div className=" p-3 lg:p-2 shadow-xl bg-white/80 rounded-full flex items-center justify-center">
           <EyeIcon
-            className="h-4 w-4 lg:h-full lg:w-full  animate-pulse  "
+            className="h-4 w-4 lg:h-6 lg:w-6  animate-pulse  "
             onClick={() => {
               navigate("/menu/:id");
             }}
